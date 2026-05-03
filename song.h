d279439b418358d5d200f231c173540aa57be86c
chawakornpukan-pixel <chawakornpukan@gmail.com>
2026-05-03 22:26:56 +0700

    Create song.h

---
+++ song.h
@@ -0,0 +1,33 @@
+#ifndef SONG_H
+#define SONG_H
+
+#define MAX_TITLE 100
+#define MAX_ARTIST 100
+#define MAX_GENRE 50
+
+typedef struct Song {
+    int id;
+    char title[MAX_TITLE];
+    char artist[MAX_ARTIST];
+    char genre[MAX_GENRE];
+    int play_count;
+    int duration;
+} Song;
+
+typedef struct Node {
+    Song song;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+typedef struct StackNode {
+    Song song;
+    struct StackNode* next;
+} StackNode;
+
+typedef struct AdjNode {
+    int song_id;
+    struct AdjNode* next;
+} AdjNode;
+
+#endif
