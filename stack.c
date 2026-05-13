050ed8d5ac07a39a1039aad468aa24fc090074a7
sutharos31-ops <sutharos.sirit@mail.kmutt.ac.th>
2026-05-13 22:02:29 +0700

    stack v.3

--- stack.c
+++ stack.c
@@ -41,15 +41,3 @@ void freeStack(Stack* stack) {
     }
     free(stack);
 }
-Peem — 3/5/69 22:56
-stack.c
-#ifndef SHUFFLE_H
-#define SHUFFLE_H
-
-#include "song.h"
-
-int getTotalWeight(Node* head);
-Node* weightedShuffle(Node* head);
-void incrementPlayCount(Node* head, int id);
-
-#endif
